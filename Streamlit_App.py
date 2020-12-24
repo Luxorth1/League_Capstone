9447babec0461c2ac12b05cbcc6abd5d7b7a10d6
unknown <steven.jasper1@gmail.com>
2020-12-23 17:58:32 -0700

    added intro

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -24,21 +24,19 @@ user_input
 games = st.sidebar.text_input(
     'How many games would you like to analyze? (Please keep this number below 10)',
     )
-if int(games) > 10:
-	st.write('PLEASE KEEP THIS NUMBER BELOW 10')
-else:
-	scaler = joblib.load('Min_Max_scaler.gz')
-	model = joblib.load('Decision_Tree.gz')
-	if st.button('Search'):
-		try:
-			if int(games) > 20:
-				st.sidebar.write('Please only analyze up to 5 games at a time.', font='red')
-			else:
-				summoner_data_x, summoner_data_y, summoner_data_raw= full_process(user_input, scaler, games)
-				game_predictions = input_model(summoner_data_x, summoner_data_y, model)
-				for i, pred in enumerate(game_predictions):
-					st.write(summoner_data_raw.iloc[i])
-					recommend(game_predictions[i], summoner_data_y[i], summoner_data_raw, i)
-		except:
-			st.write('Please validate that you have spelt your Summoner name correctly.')
-			
+
+scaler = joblib.load('Min_Max_scaler.gz')
+model = joblib.load('Decision_Tree.gz')
+if st.button('Search'):
+	try:
+		if int(games) > 20:
+			st.sidebar.write('Please only analyze up to 5 games at a time.', font='red')
+		else:
+			summoner_data_x, summoner_data_y, summoner_data_raw= full_process(user_input, scaler, games)
+			game_predictions = input_model(summoner_data_x, summoner_data_y, model)
+			for i, pred in enumerate(game_predictions):
+				st.write(summoner_data_raw.iloc[i])
+				recommend(game_predictions[i], summoner_data_y[i], summoner_data_raw, i)
+	except:
+		st.write('Please validate that you have spelt your Summoner name correctly.')
+		
